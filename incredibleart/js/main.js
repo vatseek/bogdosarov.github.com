6bcca23680eacf5dae219c054124c2c597fb3112
Bogdosarov <bogdosarov@gmail.com>
2013-06-04 17:56:03 +0300

    Edit incredible

--- incredibleart/js/main.js
+++ incredibleart/js/main.js
@@ -1,1 +1,9 @@
-
+$(function(){
+	// height for main container
+	var wrapper = $('.wrapper');
+	wrapper.height($(window).height()-161);
+	
+	$(window).resize(function() {
+		wrapper.height($(window).height()-161);			
+	});
+})
