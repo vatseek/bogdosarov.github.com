c610235869ded63a1a1be60bf46f7bda4319295b
Bogdosarov <bogdosarov@gmail.com>
2013-06-12 21:02:50 +0300

    Edit algoritm

--- incredibleart/js/main.js
+++ incredibleart/js/main.js
@@ -1,10 +1,3 @@
 $(function(){
-	// height for main container
-	var wrapper = $('.wrapper')
-		height = $(window).height()-141;
-		wrapper.css({minHeight: height+'px'});			
-	
-	$(window).resize(function() {
-		wrapper.css({minHeight: height+'px'});			
-	});
+
 })
